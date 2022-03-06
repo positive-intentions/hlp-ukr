42ca9aa4a2c57e13ff22fb2230ab90b73db62aef
raju maher <raju@factris.com>
2022-03-06 11:59:10 +0100

    latest changes

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,19 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AppDrawer from "./components/mulecules/AppDrawer";
 import Home from "./components/pages/Home";
 import Order from "./components/pages/Order";
 
-const App: FC = () => (
-  <BrowserRouter basename="/hlp-ukr">
-    <AppDrawer>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/*" element={<Navigate replace to="/home" />} />
-      </Routes>
-    </AppDrawer>
-  </BrowserRouter>
-);
+const App: FC = () => {
+  useEffect(() => {
+    if (!window.location.pathname.startsWith('/hlp-ukr')) {
+      window.location.assign('/hlp-ukr');
+    }
+  }, []);
+
+  return (
+    <BrowserRouter basename="/hlp-ukr">
+      <AppDrawer>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="*" element={<Navigate replace to="home" />} />
+        </Routes>
+      </AppDrawer>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- src/ui/components/pages/Home.tsx
+++ src/ui/components/pages/Home.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Home = (): JSX.Element => (
   <>
     <Typography variant="h3">Welcome to the Yak shop</Typography>
-    <img width="100%" src="https://source.unsplash.com/random/?yak" />
+    <img width="100%" src="https://source.unsplash.com/random/?ukraine" />
   </>
 );
 
