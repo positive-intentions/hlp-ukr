78322f337b83e80a2f31b9c1a254bf5318f5c0a2
raju maher <raju@factris.com>
2022-03-06 12:07:31 +0100

    latest changes

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,21 +1,21 @@
-import React, { FC, useEffect } from "react";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import AppDrawer from "./components/mulecules/AppDrawer";
-import Home from "./components/pages/Home";
-import Order from "./components/pages/Order";
+import React, { FC } from 'react';
+import {
+  HashRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
+import AppDrawer from './components/mulecules/AppDrawer';
+import Home from './components/pages/Home';
+import Order from './components/pages/Order';
 
-const App: FC = () => {
-  return (
-    <HashRouter>
-      <AppDrawer>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/order" element={<Order />} />
-          <Route path="*" element={<Navigate replace to="home" />} />
-        </Routes>
-      </AppDrawer>
-    </HashRouter>
-  );
-}
+const App: FC = () => (
+  <HashRouter>
+    <AppDrawer>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="*" element={<Navigate replace to="home" />} />
+      </Routes>
+    </AppDrawer>
+  </HashRouter>
+);
 
 export default App;
