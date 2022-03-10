0ba03240ad895ed3549341598740ed8b649be0ba
xoron <rmaher84@gmail.com>
2022-03-10 14:51:22 +0100

    latest changes

--- src/ui/components/pages/Calibration.tsx
+++ src/ui/components/pages/Calibration.tsx
@@ -8,7 +8,7 @@ const memoryInfo = performance.memory;
 const bytesToSize = (bytes) => {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     if (bytes === 0) return 'n/a';
-    const i = parseInt(Math.floor(Math.log(Math.abs(bytes)) / Math.log(1024)), 10);
+    const i = parseInt(`${Math.floor(Math.log(Math.abs(bytes)) / Math.log(1024))}`, 10);
     if (i === 0) return `${bytes} ${sizes[i]}`;
     return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
   }
